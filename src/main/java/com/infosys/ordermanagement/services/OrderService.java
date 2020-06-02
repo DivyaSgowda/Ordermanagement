81a6664866da639b255d13161d36ea1f18e3c3e2
DivyaSgowda <divyagowda0714@gmail.com>
2020-06-02 17:46:29 +0530

    d
    d

--- src/main/java/com/infosys/ordermanagement/services/OrderService.java
+++ src/main/java/com/infosys/ordermanagement/services/OrderService.java
@@ -42,8 +42,8 @@ public class OrderService {
 	private ProductsOrdered productsOrdered;
 	@Autowired
 	public RestTemplate restTemplate;
-	@Value("${userServiceUrl}")
-	public String userServiceUrl;
+// 	@Value("${userServiceUrl}")
+	public String userServiceUrl="abc";
 
 	public Integer[] usingRewardPoints(Integer buyerId,Integer eligibleDiscount) {
         String getrewardUrl=userServiceUrl+"rewardPoint/"+buyerId;
